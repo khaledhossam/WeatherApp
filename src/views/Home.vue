494b3d5589444f004b00ddbdb1926f62553bdca5
khaledhossam <khaledhossam@users.noreply.github.com>
2020-05-07 05:42:32 +0200

    First

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  
+  <main>
+    <div>
+      <a id="msg">
+        Hello My Name Is Khaled
+      </a>
+  
+  
+  
+    </div>
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+
 };
 </script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+#app{
+  background-image: url('../assets/background-home.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+#msg{
+
+  font-size: 150px;
+  font-style: italic;
+  color: beige;
+  
+}
+
+</style>
